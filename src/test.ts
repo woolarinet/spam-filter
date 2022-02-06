9d8f8c00833c2c8ebf8c8fb434d7c1cbdb3d9421
sunho <zerozzung@gmail.com>
2022-02-06 17:13:06 +0900

    Delete test.ts

--- src/test.ts
+++
@@ -1,5 +0,0 @@
-import SpamFilter from "./spam-filter";
-
-const filter = new SpamFilter("hd adfaifjoa dd https://goo.gl/nVLutc", ["http://www.filekok.com/main"], 3);
-
-console.log(filter.isSpam());
